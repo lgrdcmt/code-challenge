95ec1fed18dc65c43c371f884917136dad4f8220
Luiz Gabriel Righetti de Campos <lgrdc.mt@gmail.com>
2025-01-31 12:18:40 -0300

    insert airflow

---
+++ airflow/dags/terceira_dag.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+
+dag = DAG('terceira_dag', description='Terceira DAG',
+          schedule_interval=None, start_date=datetime(2025, 1, 20), 
+          catchup=False)
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag)
+
+# Task rodando em paralelo
+[task1, task2] >> task3
