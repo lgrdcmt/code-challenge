236cf9796f1670669990b9dc54bc2f566b3b9a2e
Luiz Gabriel Righetti de Campos <lgrdc.mt@gmail.com>
2025-02-04 15:05:28 -0300

    update

--- airflow/dags/dag1_extract.py
+++ airflow/dags/dag1_extract.py
@@ -11,35 +11,35 @@ dag = DAG('dag1_extract', description='First DAG using Meltano',
 
 task1 = DockerOperator(
     task_id="csv_to_csv",
-    image="meltano-project:latest",
-    command=["sh", "-c", "meltano --cwd meltano install && meltano --cwd meltano run csv_to_csv"],
+    image="meltano/meltano:v3.6.0-python3.10",
+    command=["sh", "-c", "meltano --cwd meltano run csv_to_csv"],
     entrypoint=[""],
     api_version="auto",
     auto_remove=True,
     docker_url="tcp://docker-socket-proxy:2375",
     network_mode="bridge",
     mount_tmp_dir=False,
-    mounts=[Mount(source="/home/gabriel/Downloads/git/code-challenge", target="/project", type="bind", read_only=False)],
+    mounts=[Mount(source="/Users/gabriel/Cursos/desafio_indicium/code-challenge", target="/project", type="bind", read_only=False)],
     environment={"EXEC_DATE": "{{ds}}"},
     dag=dag
 )
 
 task2 = DockerOperator(
     task_id="postgres_to_csv",
-    image="meltano-project:latest",
-    command=["sh", "-c", "meltano --cwd meltano install && meltano --cwd meltano run postgres_to_csv"],
+    image="meltano/meltano:v3.6.0-python3.10",
+    command=["sh", "-c", "meltano --cwd meltano run postgres_to_csv"],
     entrypoint=[""],
     api_version="auto",
     auto_remove=True,
     docker_url="tcp://docker-socket-proxy:2375",
     network_mode="bridge",
     mount_tmp_dir=False,
-    mounts=[Mount(source="/home/gabriel/Downloads/git/code-challenge", target="/project", type="bind", read_only=False)],
+    mounts=[Mount(source="/Users/gabriel/Cursos/desafio_indicium/code-challenge", target="/project", type="bind", read_only=False)],
     environment={"EXEC_DATE": "{{ds}}"},
     dag=dag
 )
 
-task3 = TriggerDagRunOperator(task_id='trigger_dag2', trigger_dag_id='dag2_loadertransform',logical_date="{{ds}}", dag=dag)
+task3 = TriggerDagRunOperator(task_id='trigger_dag2', trigger_dag_id='dag2_loadertransform',logical_date="{{ds}}", deferrable=True, dag=dag)
 
 
 task1 >> task2  >> task3
--- airflow/dags/dag2_loadertransform.py
+++ airflow/dags/dag2_loadertransform.py
@@ -10,15 +10,15 @@ dag = DAG('dag2_loadertransform', description='First DAG using Meltano',
 
 task1 = DockerOperator(
     task_id="loader_transform",
-    image="meltano-project:latest",
-    command=["sh", "-c", "meltano --cwd meltano install && meltano --cwd meltano invoke dbt-postgres:build && meltano --cwd meltano run files_to_postgres"],
+    image="meltano/meltano:v3.6.0-python3.10",
+    command=["sh", "-c", "meltano --cwd meltano run files_to_postgres && meltano --cwd meltano invoke dbt-postgres:build"],
     entrypoint=[""],
     api_version="auto",
     auto_remove=True,
     docker_url="tcp://docker-socket-proxy:2375",
     network_mode="bridge",
     mount_tmp_dir=False,
-    mounts=[Mount(source="/home/gabriel/Downloads/git/code-challenge", target="/project", type="bind", read_only=False)],
+    mounts=[Mount(source="/Users/gabriel/Cursos/desafio_indicium/code-challenge", target="/project", type="bind", read_only=False)],
     environment={"EXEC_DATE": "{{ds}}",
                  "DBT_POSTGRES_PASSWORD":"102030"},
     dag=dag
@@ -27,15 +27,15 @@ task1 = DockerOperator(
 
 task2 = DockerOperator(
     task_id="export_final",
-    image="meltano-project:latest",
-    command=["sh", "-c", "meltano --cwd meltano install && meltano --cwd meltano run export_final"],
+    image="meltano/meltano:v3.6.0-python3.10",
+    command=["sh", "-c", "meltano --cwd meltano run export_final"],
     entrypoint=[""],
     api_version="auto",
     auto_remove=True,
     docker_url="tcp://docker-socket-proxy:2375",
     network_mode="bridge",
     mount_tmp_dir=False,
-    mounts=[Mount(source="/home/gabriel/Downloads/git/code-challenge", target="/project", type="bind", read_only=False)],
+    mounts=[Mount(source="/Users/gabriel/Cursos/desafio_indicium/code-challenge", target="/project", type="bind", read_only=False)],
     environment={"EXEC_DATE": "{{ds}}"},
     dag=dag
 )
